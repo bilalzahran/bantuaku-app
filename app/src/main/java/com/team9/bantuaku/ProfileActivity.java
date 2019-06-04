e52f807ea58984f4c7492612c1212a2fce6cab6f
Maulidito <maulidito@gmail.com>
2019-06-04 16:50:09 +0700

    menambahkan front-end Posting

--- app/src/main/java/com/team9/bantuaku/ProfileActivity.java
+++ app/src/main/java/com/team9/bantuaku/ProfileActivity.java
@@ -3,13 +3,34 @@ package com.team9.bantuaku;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private CircleImageView poto_profile;
+    private TextView point_profile;
+    private TextView selesai_profile;
+    private TextView biografi;
+    private TextView keahlian;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        poto_profile = findViewById(R.id.poto_profile);
+        point_profile = findViewById(R.id.poin_profile);
+        selesai_profile = findViewById(R.id.selesai_profile);
+        biografi = findViewById(R.id.biografi);
+        keahlian = findViewById(R.id.keahlian);
+        initdummy();
 
     }
+
+    public void initdummy(){
+        point_profile.setText("999");
+        selesai_profile.setText("999");
+        biografi.setText("ini hanyalah init dummy jadi gak ada apa apa yo wess ini percobaan kecil sadja ");
+        keahlian.setText("ini hanyalah init dummy jadi gak ada apa apa yo wess ini percobaan kecil sadja ");
+    }
+
 }
