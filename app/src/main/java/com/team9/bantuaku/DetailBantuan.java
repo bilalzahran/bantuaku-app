9ec04136f46ba24b5fd7365fce5639cfaa994d71
bilalzahran <bilalzahran08@gmail.com>
2019-07-19 07:37:03 +0700

    Add Detail bantuan layout

--- app/src/main/java/com/team9/bantuaku/DetailBantuan.java
+++ app/src/main/java/com/team9/bantuaku/DetailBantuan.java
@@ -11,9 +11,12 @@ public class DetailBantuan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_bantuan);
+
         //Toolbar init
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
     }
 }
